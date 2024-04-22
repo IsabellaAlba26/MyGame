ca443cab7ca9d26368ae2f45ad417f026c8ddb47
Isabella Alba <isabella@isabellas-mbp.milton.edu>
2024-04-22 08:19:07 -0400

    levels not functioning yet

--- src/GameLand.java
+++ src/GameLand.java
@@ -48,12 +48,26 @@ public class GameLand implements Runnable, KeyListener {
     public BufferStrategy bufferStrategy;
 
     //
-    public boolean startScreen=true;
-    public boolean isPlaying;
-    public boolean gameOver=false;
+    public boolean startScreen = true;
+    public boolean showRulesScreen = false;
+    public boolean level1Screen = false;
+    public boolean level1PlayingScreen;
+    public boolean level2Screen;
+    public boolean level2PlayingScreen;
+    public boolean level3Screen;
+    public boolean level3PlayingScreen;
+    public boolean level4Screen;
+    public boolean level4PlayingScreen;
+    public boolean level5Screen;
+    public boolean level5PlayingScreen;
+    public boolean gameOver = false;
+    public boolean showWinningScreen;
+
 
     //Declare the objects used in the program below
-    /** STEP 1: Declare your object and give it a name **/
+    /**
+     * STEP 1: Declare your object and give it a name
+     **/
     public Hero cat;
     public RegularMice[] mouse;
     public Hero goldenMouse;
@@ -64,7 +78,9 @@ public class GameLand implements Runnable, KeyListener {
     public long elapsedTime;
 
 
-    /**STEP 2: Declare an image for your object**/
+    /**
+     * STEP 2: Declare an image for your object
+     **/
     public Image homescreen;
     public Image rulesScreen;
     public Image catPic;
@@ -81,6 +97,7 @@ public class GameLand implements Runnable, KeyListener {
     public Image level4screen;
     public Image level5screen;
     public Image losingScreen;
+    public Image winningScreen;
 
     // Main method definition: PSVM
     // This is the code that runs first and automatically
@@ -99,40 +116,160 @@ public class GameLand implements Runnable, KeyListener {
 
         //create (construct) the objects needed for the game below
         //for each object that has a picture, load in images as well
-    /**STEP 3: construct a specific Hero**/
-    cat = new Hero(5000,5000,20,20 ,250,166,true);
-    //the start time will need to move to when levels start
-    startTime=System.currentTimeMillis();
-    mouse= new RegularMice[75];
-        for(int x=0; x<mouse.length; x=x+1){
-            mouse[x] = new RegularMice((int)(Math.random()*2100),(int)(Math.random()*1801),(int)(Math.random()*5.1),(int)(Math.random()*5.1),100,71,true);
-        }
-    goldenMouse = new Hero(500,500,4,3,100,71,true);
+        /**STEP 3: construct a specific Hero**/
+        goldenMouse = new Hero(500, 500, 4, 3, 100, 71, true);
 
 
         /**STEP 4: load in the image for your object**/
-        homescreen=Toolkit.getDefaultToolkit().getImage("Homescreen.png");
-        rulesScreen=Toolkit.getDefaultToolkit().getImage("rules screen.png");
-        catPic=Toolkit.getDefaultToolkit().getImage("cat.png");
-        mousePic=Toolkit.getDefaultToolkit().getImage("mouse.png");
-        goldenMousePic=Toolkit.getDefaultToolkit().getImage("golden mouse.png");
-        attic=Toolkit.getDefaultToolkit().getImage("attic.jpeg");
-        bathroom=Toolkit.getDefaultToolkit().getImage("bathroom.jpeg");
-        bedroom=Toolkit.getDefaultToolkit().getImage("bedroom.jpeg");
-        kitchen=Toolkit.getDefaultToolkit().getImage("kitchen.jpeg");
-        livingRoom=Toolkit.getDefaultToolkit().getImage("living room.jpeg");
-        level1screen=Toolkit.getDefaultToolkit().getImage("LEVEL 1 screen.png");
-        level2screen=Toolkit.getDefaultToolkit().getImage("Level 2 screen.png");
-        level3screen=Toolkit.getDefaultToolkit().getImage("Level 3 screen.png");
-        level4screen=Toolkit.getDefaultToolkit().getImage("Level 4 screen.png");
-        level5screen=Toolkit.getDefaultToolkit().getImage("Level 5 screen.png");
-        losingScreen=Toolkit.getDefaultToolkit().getImage("Losing screen.png");
-
-
+        homescreen = Toolkit.getDefaultToolkit().getImage("Homescreen.png");
+        rulesScreen = Toolkit.getDefaultToolkit().getImage("rules screen.png");
+        catPic = Toolkit.getDefaultToolkit().getImage("cat.png");
+        mousePic = Toolkit.getDefaultToolkit().getImage("mouse.png");
+        goldenMousePic = Toolkit.getDefaultToolkit().getImage("golden mouse.png");
+        attic = Toolkit.getDefaultToolkit().getImage("attic.jpeg");
+        bathroom = Toolkit.getDefaultToolkit().getImage("bathroom.jpeg");
+        bedroom = Toolkit.getDefaultToolkit().getImage("bedroom.jpeg");
+        kitchen = Toolkit.getDefaultToolkit().getImage("kitchen.jpeg");
+        livingRoom = Toolkit.getDefaultToolkit().getImage("living room.jpeg");
+        level1screen = Toolkit.getDefaultToolkit().getImage("LEVEL 1 screen.png");
+        level2screen = Toolkit.getDefaultToolkit().getImage("Level 2 screen.png");
+        level3screen = Toolkit.getDefaultToolkit().getImage("Level 3 screen.png");
+        level4screen = Toolkit.getDefaultToolkit().getImage("Level 4 screen.png");
+        level5screen = Toolkit.getDefaultToolkit().getImage("Level 5 screen.png");
+        losingScreen = Toolkit.getDefaultToolkit().getImage("Losing screen.png");
+        winningScreen = Toolkit.getDefaultToolkit().getImage("winning screen.png");
 
 
     }// GameLand()
 
+    public void runCorrectLevel() {
+        // call levels based on booleans
+        if (startScreen) {
+            startScreen = false;
+            showRulesScreen = true;
+            System.out.println("now show the rules screen");
+        }
+
+        if (showRulesScreen) {
+            showRulesScreen = false;
+            level1Screen = true;
+            System.out.println("now show the level 1 screen");
+        }
+
+        if (level1Screen) {
+            level1Screen = false;
+            startLevel1();
+            level1PlayingScreen = true;
+        }
+        if (level1PlayingScreen && score == 30 && elapsedTime >= 0) {
+            level1PlayingScreen = false;
+            level2Screen = true;
+        }
+        if (level1PlayingScreen && score < 30 && elapsedTime == 50) {
+            gameOver = true;
+        }
+        if (level2Screen) {
+            level2Screen = false;
+            level2PlayingScreen = true;
+            startLevel2();
+        }
+        if (level2PlayingScreen && score == 30 && elapsedTime >= 0) {
+            level2PlayingScreen = false;
+            level3Screen = true;
+        }
+        if (level2PlayingScreen && score < 30 && elapsedTime == 45) {
+            gameOver = true;
+        }
+        if (level3Screen) {
+            level3Screen = false;
+            level3PlayingScreen = true;
+            startLevel3();
+        }
+        if (level3PlayingScreen && score == 30 && elapsedTime >= 0) {
+            level3PlayingScreen = false;
+            level4Screen = true;
+        }
+        if (level3PlayingScreen && score < 30 && elapsedTime == 40) {
+            gameOver = true;
+        }
+        if (level4Screen) {
+            level4Screen = false;
+            level4PlayingScreen = true;
+            startLevel4();
+        }
+        if (level4PlayingScreen && score == 30 && elapsedTime >= 0) {
+            level4PlayingScreen = false;
+            level5Screen = true;
+        }
+        if (level4PlayingScreen && score < 30 && elapsedTime == 30) {
+            gameOver = true;
+        }
+        if (level5Screen) {
+            level5Screen = false;
+            level5PlayingScreen = true;
+            startLevel5();
+        }
+        if (level5PlayingScreen && score == 30 && elapsedTime >= 0) {
+            level5PlayingScreen = false;
+            showWinningScreen=true;
+        }
+        if (level5PlayingScreen && score < 30 && elapsedTime == 20) {
+            gameOver = true;
+        }
+    }
+
+    public void startLevel1() {
+        cat = new Hero(200, 400, 20, 20, 250, 166, true);
+        //the start time will need to move to when levels start
+        startTime = System.currentTimeMillis();
+        mouse = new RegularMice[75];
+        for (int x = 0; x < mouse.length; x = x + 1) {
+            mouse[x] = new RegularMice((int) (Math.random() * 2100), (int) (Math.random() * 1801), (int) (Math.random() * 5.1), (int) (Math.random() * 5.1), 100, 71, true);
+        }
+    }
+
+    public void startLevel2() {
+        //reset time
+        startTime = System.currentTimeMillis();
+        //reset score
+        score = 0;
+    }
+
+    public void startLevel3() {
+        //reset time
+        startTime = System.currentTimeMillis();
+        //reset score
+        score = 0;
+        //new mice
+        mouse = new RegularMice[100];
+        for (int x = 0; x < mouse.length; x = x + 1) {
+            mouse[x] = new RegularMice((int) (Math.random() * 2100), (int) (Math.random() * 1801), (int) (Math.random() * 5.1), (int) (Math.random() * 5.1), 100, 71, true);
+        }
+    }
+
+    public void startLevel4(){
+        //reset time
+        startTime=System.currentTimeMillis();
+        //reset score
+        score=0;
+        //new mice
+        mouse = new RegularMice[125];
+        for (int x = 0; x < mouse.length; x = x + 1) {
+            mouse[x] = new RegularMice((int) (Math.random() * 2100), (int) (Math.random() * 1801), (int) (Math.random() * 5.1), (int) (Math.random() * 5.1), 100, 71, true);
+        }
+    }
+
+    public void startLevel5(){
+        //reset time
+        startTime=System.currentTimeMillis();
+        //reset score
+        score=0;
+        //new mice
+        mouse = new RegularMice[135];
+        for (int x = 0; x < mouse.length; x = x + 1) {
+            mouse[x] = new RegularMice((int) (Math.random() * 2100), (int) (Math.random() * 1801), (int) (Math.random() * 5.1), (int) (Math.random() * 5.1), 100, 71, true);
+        }
+    }
 //*******************************************************************************
 //User Method Section
 //
@@ -150,8 +287,25 @@ public class GameLand implements Runnable, KeyListener {
             //getting current time`
             currentTime = System.currentTimeMillis();
             //calculate elapsed time
-            elapsedTime = (int) (50 - (currentTime - startTime) * .001);
+            if(level1PlayingScreen) {
+                elapsedTime = (int) (50 - (currentTime - startTime) * .001);
+            }
+            if(level2PlayingScreen){
+                elapsedTime = (int) (45 - (currentTime - startTime) * .001);
 
+            }
+            if(level3PlayingScreen){
+                elapsedTime = (int) (40 - (currentTime - startTime) * .001);
+
+            }
+            if(level4PlayingScreen){
+                elapsedTime = (int) (30 - (currentTime - startTime) * .001);
+
+            }
+            if(level5PlayingScreen){
+                elapsedTime = (int) (20 - (currentTime - startTime) * .001);
+
+            }
         }
     }
 
@@ -160,41 +314,73 @@ public class GameLand implements Runnable, KeyListener {
         Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
         g.clearRect(0, 0, WIDTH, HEIGHT);
 
-        if(startScreen==true){
+        if(startScreen){
             g.drawImage(homescreen, 0, 0, WIDTH, HEIGHT, null);
         }
+        if(showRulesScreen) {
+            g.drawImage(rulesScreen, 0, 0, WIDTH, HEIGHT, null);
+        }
+        if(level1Screen){
+            g.drawImage(level1screen,0,0,WIDTH, HEIGHT,null);
+        }
+        if(level1PlayingScreen){
+            g.drawImage(livingRoom, 0, 0, WIDTH, HEIGHT, null);
+            g.drawString("Mice Killed: " + score, 100, 100);
+            g.drawString("Time left:" + elapsedTime, 900, 100);
+        }
+        if(level2Screen){
+            g.drawImage(level2screen, 0, 0, WIDTH, HEIGHT, null);
+        }
+        if(level2PlayingScreen){
+            g.drawImage(kitchen,0,0,WIDTH, HEIGHT,null);
+            g.drawString("Mice Killed: " + score, 100, 100);
+            g.drawString("Time left:" + elapsedTime, 900, 100);
+        }
+        if(level3Screen){
+            g.drawImage(level3screen,0,0,WIDTH, HEIGHT,null);
+        }
+        if(level3PlayingScreen){
+            g.drawImage(bathroom,0,0,WIDTH, HEIGHT,null);
+            g.drawString("Mice Killed: " + score, 100, 100);
+            g.drawString("Time left:" + elapsedTime, 900, 100);
+        }
+        if(level4Screen){
+            g.drawImage(level4screen,0,0,WIDTH, HEIGHT,null);
+        }
+        if(level4PlayingScreen){
+            g.drawImage(bedroom,0,0,WIDTH, HEIGHT,null);
+            g.drawString("Mice Killed: " + score, 100, 100);
+            g.drawString("Time left:" + elapsedTime, 900, 100);
+        }
+        if(level5Screen){
+            g.drawImage(level5screen,0,0,WIDTH, HEIGHT,null);
+        }
+        if(level5PlayingScreen){
+            g.drawImage(attic,0,0,WIDTH, HEIGHT,null);
+            g.drawString("Mice Killed: " + score, 100, 100);
+            g.drawString("Time left:" + elapsedTime, 900, 100);
+        }
 
-        if(isPlaying==true){
-        //draw the image of your objects below:
-        /** STEP 5: draw the image of your object on the screen**/
-            g.drawImage(homescreen, 0, 0, WIDTH, HEIGHT, null);
-        g.drawImage(rulesScreen, 0, 0, WIDTH, HEIGHT, null);
-        g.drawImage(livingRoom, 0, 0, WIDTH, HEIGHT, null);
-        g.drawString("Mice Killed: " + score, 100, 100);
-        g.drawString("Time left:" + elapsedTime, 900, 100);
-
-        g.drawImage(catPic, cat.xpos, cat.ypos, cat.width, cat.height, null);
-        g.drawImage(goldenMousePic, goldenMouse.xpos, goldenMouse.ypos, goldenMouse.width, goldenMouse.height, null);
-
-        for (int x = 0; x < mouse.length; x = x + 1) {
-            if (mouse[x].isAlive == true) {
-                g.drawImage(mousePic, mouse[x].xpos, mouse[x].ypos, mouse[x].width, mouse[x].height, null);
+            //draw the image of your objects below:
+            /** STEP 5: draw the image of your object on the screen**/
+            if (cat!=null) {
+                g.drawImage(catPic, cat.xpos, cat.ypos, cat.width, cat.height, null);
             }
-        }
+            g.drawImage(goldenMousePic, goldenMouse.xpos, goldenMouse.ypos, goldenMouse.width, goldenMouse.height, null);
+            if(mouse!=null) {
+                for (int x = 0; x < mouse.length; x = x + 1) {
+                    if (mouse[x].isAlive == true) {
+                        g.drawImage(mousePic, mouse[x].xpos, mouse[x].ypos, mouse[x].width, mouse[x].height, null);
+                    }
+                }
+            }
 
-        // g.drawImage(attic,0,0,WIDTH, HEIGHT,null);
-        // g.drawImage(bathroom,0,0,WIDTH, HEIGHT,null);
-        // g.drawImage(bedroom,0,0,WIDTH, HEIGHT,null);
-        // g.drawImage(kitchen,0,0,WIDTH, HEIGHT,null);
-        // g.drawImage(level1screen,0,0,WIDTH, HEIGHT,null);
-        //g.drawImage(level2screen, 0, 0, WIDTH, HEIGHT, null);
-        // g.drawImage(level3screen,0,0,WIDTH, HEIGHT,null);
-        // g.drawImage(level4screen,0,0,WIDTH, HEIGHT,null);
-        // g.drawImage(level5screen,0,0,WIDTH, HEIGHT,null);
-    }
 
-        if(gameOver==true){
+        if(gameOver){
             g.drawImage(losingScreen,0,0,WIDTH, HEIGHT,null);
+        }
+        if(showWinningScreen){
+            g.drawImage(winningScreen,0,0,WIDTH, HEIGHT,null);
         }
 
 
@@ -206,18 +392,24 @@ public class GameLand implements Runnable, KeyListener {
 
     public void moveThings() {
         //call the move() method code from your object class
-        cat.move(5);
-        for(int x=0; x<mouse.length; x=x+1){
-            mouse[x].bouncingMove();
+        if(cat!=null){ //cat!=null means if cat is not empty
+            cat.move(5);
+        }
+        if(mouse!=null) {
+            for (int x = 0; x < mouse.length; x = x + 1) {
+                mouse[x].bouncingMove();
+            }
         }
     }
 
     public void collisions(){
-        for(int x=0; x<mouse.length; x=x+1){
-        if(mouse[x].rec.intersects(cat.rec) && mouse[x].isAlive==true){
-            mouse[x].isAlive=false;
-            score=score+1;
-        }
+        if(mouse!=null && cat!=null){
+            for (int x = 0; x < mouse.length; x = x + 1) {
+                if (mouse[x].rec.intersects(cat.rec) && mouse[x].isAlive == true) {
+                    mouse[x].isAlive = false;
+                    score = score + 1;
+                }
+            }
         }
 
     }
@@ -266,7 +458,7 @@ public class GameLand implements Runnable, KeyListener {
     }
 
 
-//all three key Listener methods must be included in the code to avoid errors
+    //all three key Listener methods must be included in the code to avoid errors
     @Override
     public void keyTyped(KeyEvent e) {
         //leave empty
@@ -274,8 +466,8 @@ public class GameLand implements Runnable, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-    char key =e.getKeyChar();
-    int keyCode = e.getKeyCode();
+        char key =e.getKeyChar();
+        int keyCode = e.getKeyCode();
         System.out.println("Key Character: " + key + " KeyCode: "+ keyCode);
 
         if(keyCode==40){
@@ -325,12 +517,10 @@ public class GameLand implements Runnable, KeyListener {
         }
 
         if(keyCode==10){ //keyCode 10 is enter/return key
-            startScreen=false;
-            isPlaying=true;
-            cat.xpos=205;
-            cat.ypos=222;
+            runCorrectLevel();
+            System.out.println("10 is pressed");
         }
-        
+
 
     }
 }
